01bc17ba2777e24297f9810cb0b916f799f5dcd3
Dve Yarangi <dveyarangi@gmail.com>
2016-10-19 17:49:40 +0300

    faster thread downloading

---
+++ java/src/Console.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+public class Console
+{
+	public Console()
+	{
+
+	}
+
+	public void show()
+	{
+		JFrame frame = new JFrame("FrameDemo");
+
+		//2. Optional: What happens when the frame closes?
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//3. Create components and put them in the frame.
+		//...create emptyLabel...
+		//frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+		//4. Size the frame.
+		frame.pack();
+
+		//5. Show it.
+		frame.setVisible(true);
+	}
+}
---
+++ java/src/DataStamp.java
@@ -0,0 +1,7 @@
+public class DataStamp {
+	String path = "junk";
+	String time;
+	String date;
+	public DataStamp() {}
+	public DataStamp(String time, String date) { this.time = time; this.date = date; }
+}
