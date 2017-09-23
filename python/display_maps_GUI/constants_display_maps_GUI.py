dcdc76151e78cfec544213e0a80dda9af38014a9
hatzv <hatzvika@gmail.com>
2017-09-23 16:33:40 +0800

    The first version of the maps GUI. The 3 files are the main file (icing_probability_Gui.py), the map creation function (calculate_icing_probability) and the constants file.

---
+++ python/display_maps_GUI/constants_display_maps_GUI.py
@@ -0,0 +1,49 @@
+################################################
+# This file contains the GUI's constants
+################################################
+
+# Defaults
+default_hPa_level = '700'
+default_lanlot_cs = '40'
+default_min_LWC = '0'
+default_file_path = 'D:/DB/Era_Interim'
+default_year = '1996'
+default_month = '01'
+default_day = '03'
+default_hour = '18'
+
+# Strings constants
+title = 'Icing Probability GUI'
+
+data_type_label = 'Data Type'
+data_type_1 = 'Icing Probability'
+data_type_2 = 'Liquid Water Content'
+data_type_3 = 'Relative Humidity'
+data_type_4 = 'Relative Humidity Probability'
+data_type_5 = 'Temperature'
+data_type_6 = 'Temperature Probability'
+
+cross_section_label = 'Cross Section'
+cross_section_1 = 'GPH level'
+cross_section_2 = 'Latitude'
+cross_section_3 = 'Longitude'
+
+hPa_level_label = 'hPa level'
+
+latlon_label_1 = 'Lat/Lon'
+latlon_label_2 = 'Lat'
+latlon_label_3 = 'Lon'
+
+min_LWC_label = 'Min LWC'
+
+files_path_label = 'Files Path'
+
+date_label = 'Date'
+year_label = 'Year'
+month_label = 'Month'
+day_label = 'Day'
+hour_label = 'Hour'
+
+detached_map_checkbutton = 'Detached Map'
+
+draw_button_text = 'Draw map'
